1698e88680dd0b72b5e53ab325752916b706d78d
Robin Hooda <robinhooda66@gmail.com>
2019-09-11 12:20:47 +0530

    stored and accessed array in localstorage

--- 10-9-2019/json/js.js
+++ 10-9-2019/json/js.js
@@ -1,17 +1,37 @@
 $(function(){
     // data in attributes
     // getAttributeData()
-    getJSON()
-    // getLocalData()
+   //getJSON();
+    getLocalData()
+   // console.log("Good morning")
  })
- function getJSON(){
-    $.getJSON('ques-db.json',function(json){
-        questions = json;
-        // console.log(Object)
-        const keys = Object.keys(questions)
-        console.log(keys.length);
-        // [0 -> 1] * 1
-        let randIndex = Math.round(Math.random()*(keys.length-1))
-        // console.log(randIndex);
-    })
- }
+//  function getJSON(){
+//      console.log("hi");
+//     $.getJSON('ques-db.json',function(){
+//          let questions = json;
+//         console.log(Object)
+//         console.log("hi");
+//         const keys = Object.keys(questions)
+//         console.log(keys.length);
+//         // [0 -> 1] * 1
+//         let randIndex = Math.floor(Math.round(Math.random()*(keys.length-1)));
+//         // console.log(randIndex);
+//     })
+//  }
+//  let users=[];
+//  users.push("robin");
+//  users.push("rob");
+//  users.push("akash");
+//  let Jsonusers=JSON.stringify(users);
+function getLocalData(){
+  //  let names =['robin','rohan','mohan'];
+    var array = [1, 2, 3];
+
+    localStorage.setItem("array", JSON.stringify(array));
+    array = JSON.parse(localStorage.getItem("array"));
+
+    console.log(array);
+    console.log(array[1]);
+
+
+}
