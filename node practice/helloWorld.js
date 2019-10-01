47613dc0be02aea1bd85623f2f57c3cd554093f6
Robin Hooda <robinhooda66@gmail.com>
2019-10-01 13:45:42 +0530

    splice

--- node practice/helloWorld.js
+++ node practice/helloWorld.js
@@ -1,5 +1,11 @@
-console.log('hello world');
-name="robin"
-console.log(name);
-name="king"
-console.log(name);
+// console.log('hello world');
+// name="robin"
+// console.log(name);
+// name="king"
+// console.log(name);
+
+let numbers=['one','two','three'];
+
+console.log(numbers);
+numbers.splice(1,2,'robin','hooda','qwerty')
+console.log(numbers);
