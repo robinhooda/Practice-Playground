d406c4a7552f89c7f4ad85260e12084ddf442109
Robin Hooda <robinhooda66@gmail.com>
2019-10-01 18:30:18 +0530

    function

--- node practice/practiceSheet.js
+++ node practice/practiceSheet.js
@@ -3,9 +3,28 @@
 // console.log(name);
 // name="king"
 // console.log(name);
+ 
+// let numbers=['one','two','three'];
+// console.log(numbers);
+// numbers.splice(1,2,'robin','hooda','qwerty')
+// console.log(numbers);
 
-let numbers=['one','two','three'];
+// let sayHello= function(name) {
+//     console.log(`hii ${name}`);
+// }
+// sayHello('robin')
 
-console.log(numbers);
-numbers.splice(1,2,'robin','hooda','qwerty')
-console.log(numbers);
+// let fullName = function(firstName,lastName){
+//     console.log("Welcome to programming");
+//     console.log(`Mr. ${firstName} ${lastName}`);
+// }
+// fullName('Robin','Hooda');
+
+let add = function(num1,num2){
+    let sum=num1+num2;
+    return sum;
+}
+let result=add(7,134)
+console.log(`the answer is ${result}`)
+
+
