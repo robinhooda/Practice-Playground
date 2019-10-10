f6acd2e1be15d3a9fb95e4861fb779febb3aff23
Robin Hooda <robinhooda66@gmail.com>
2019-10-10 21:37:26 +0530

    todo application using foreach and for loop

--- node practice/trello.js
+++ node practice/trello.js
@@ -1,14 +1,29 @@
-const days=['Mon','Tue','Wed','Thrs','fri','Sat','Sun']
+// const days=['Mon','Tue','Wed','Thrs','fri','Sat','Sun']
 
-days.forEach(function(day,index){
-    console.log(`Day ${index+1}: ${day}`);
-})
+// days.forEach(function(day,index){
+//     console.log(`Day ${index+1}: ${day}`);
+// })
 
 
-// for(let index=0; index< days.length; index++){
+// // for(let index=0; index< days.length; index++){
+// //     console.log(days[index]);
+// // }
+
+// for(let index=days.length-1; index>=0; index--){
 //     console.log(days[index]);
 // }
+ const myTodos = []
 
-for(let index=days.length-1; index>=0; index--){
-    console.log(days[index]);
+ myTodos.push('Complete Css tutorial')
+ myTodos.push('Complete Jquery')
+ myTodos.push('Go to Gym')
+
+ //using foreach loop
+ myTodos.forEach(function(task, index){
+     console.log(`Your task no. ${index+1} is: ${task}`);
+ })
+
+ //using for loop
+for(let index=myTodos.length; index>0;index--){
+    console.log(`Your task no. ${index} is: ${myTodos[index-1]}`);
 }
