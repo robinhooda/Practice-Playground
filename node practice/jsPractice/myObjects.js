877320496fd39b3620b9b2cd852046ef3e694ac3
Robin Hooda <robinhooda66@gmail.com>
2019-10-17 18:02:52 +0530

    function using object

--- node practice/jsPractice/myObjects.js
+++ node practice/jsPractice/myObjects.js
@@ -3,5 +3,17 @@ let studentId = {
     rollno: 34,
     Address: "Haryana"
 }
-console.log(studentId);
-console.log(`Name of Student : ${studentId.name} and  he lives in ${studentId.Address}, bearing roll no. ${studentId.rollno}`)
+// console.log(studentId);
+
+// console.log(`Name of Student : ${studentId.name} and  he lives in ${studentId.Address}, bearing roll no. ${studentId.rollno}`)
+
+let edit = function(myObject){
+    return `Name of student is ${myObject.name}`
+}
+console.log(edit(studentId));
+
+// let list = [1, 2, 3, 4,5,1,3,9,22,1,6,7];
+// // list2 = [4, 2, 7, 0];
+// // let list3 = [...list, ...list2];
+// let sorted = list.sort((a, b) => a - b);
+// console.log(sorted);
