5081deb894a5c992b92fb3072e3e3338942002d4
Robin Hooda <robinhooda66@gmail.com>
2019-10-17 12:32:41 +0530

    using objects

--- node practice/jsPractice/myObjects.js
+++ node practice/jsPractice/myObjects.js
@@ -3,4 +3,5 @@ let studentId = {
     rollno: 34,
     Address: "Haryana"
 }
-console.log(studentId)
+console.log(studentId);
+console.log(`Name of Student : ${studentId.name} and  he lives in ${studentId.Address}, bearing roll no. ${studentId.rollno}`)
