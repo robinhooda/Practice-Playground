db73b3cdcca4cbbee583253aeecc4e278fdfd4d8
Robin Hooda <robinhooda66@gmail.com>
2019-11-02 11:55:56 +0530

    updated

---
+++ node practice/node api/routes/note_routes.js
@@ -0,0 +1,8 @@
+module.exports= function(app, db){
+    module.exports = function(app, db) {
+          app.post('/notes', (req, res) => {
+                  // You'll create your note here.    
+                  res.send('Hello')  
+           });
+    };
+};
