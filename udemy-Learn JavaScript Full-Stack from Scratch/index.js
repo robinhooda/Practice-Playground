65cd9299b7ee6241309d1890f0cbe126bffb51b6
Robin Hooda <robinhooda66@gmail.com>
2019-11-13 03:36:53 +0530

    example function that returns a function

--- udemy-Learn JavaScript Full-Stack from Scratch/index.js
+++ udemy-Learn JavaScript Full-Stack from Scratch/index.js
@@ -1,9 +1,9 @@
 // high order functions
     //high order function that accepts a function as an argument
-        document.addEventListener("click",clickFunction)
-        function clickFunction(){
-            alert("Cant click")
-        }
+        // document.addEventListener("click",clickFunction)
+        // function clickFunction(){
+        //     alert("Cant click")
+        // }
 
     // function doubleMe(x){
     //     return x*2
@@ -14,6 +14,20 @@
     // function quadrupleMe(x){
     //     return x*4
     // }
+// example function that returns a function
+    function createMultiplier(multiplier){
+        return function(x){
+            return x* multiplier
+        }
+    }
 
+    let doubleMe = createMultiplier(2)
+    let tripleMe = createMultiplier(3)
+    let quadrupleMe = createMultiplier(4)
+
+    document.write(doubleMe(10)+"<br>")
+    document.write(tripleMe(10))
+    document.write(quadrupleMe(10))
+    
 
 
