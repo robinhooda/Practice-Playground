810946ed83e2118cfc7c0a43d4daa73ead963709
Robin Hooda <robinhooda66@gmail.com>
2019-11-13 16:45:32 +0530

    logical operators

---
+++ udemy-Learn JavaScript Full-Stack from Scratch/logical operators/index.js
@@ -0,0 +1,33 @@
+// const password = 'r@loB@1'
+// let name='Rob H'
+
+
+// if(password.length>14 && password.includes('@') )
+// {
+//     console.log("password is very strong");
+// }
+// else if(password.length>6 || (password.includes('@') && password.length>4)){
+//     console.log("password is strong ");
+// }else{
+//     console.log("password is not strong")
+// }
+// console.log(password.length)
+// console.log(password.toUpperCase());
+// console.log(password.lastIndexOf('@'))
+// console.log(name.length)
+
+let email = "robin@gmail.com";
+//console.log(email.slice(2));
+// console.log(ex);
+
+// let result = email.slice(2,7);
+let numberu="0123456789";
+let result = numberu.slice(4,7);
+let sub = numberu.substr(2,7);
+console.log(result);
+console.log(sub);
+console.log(numberu.replace('1','2'));
+
+
+
+
