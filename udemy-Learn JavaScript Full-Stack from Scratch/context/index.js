75e4998d6dcf8b583d6ecaee0540242f7b61cb24
Robin Hooda <robinhooda66@gmail.com>
2019-11-19 05:17:45 +0530

    call function

--- udemy-Learn JavaScript Full-Stack from Scratch/context/index.js
+++ udemy-Learn JavaScript Full-Stack from Scratch/context/index.js
@@ -1,11 +1,47 @@
+// let orderData = {
+//      orderNumber: 234566,
+//      customerName: "Robin",
+//      customerAddress: "HSR layout sector-4",
+//      customerMessageOnSuccessfullDelivery(){
+//           console.log("Order No: "+ this.orderNumber+" is delivered to "+this.customerName+" at "+this.customerAddress);
+//      }
+// }
+// orderData.customerMessageOnSuccessfullDelivery();
+
+
+
+// let orderData = {
+//      orderNumber: 234566,
+//      customerName: "Robin",
+//      customerAddress: "HSR layout sector-4",
+//      customerMessageOnSuccessfullDelivery(){
+//           function tryThis(){
+//                console.log(this); // represnts the whole window in which all the web elements are available
+//           }
+//           tryThis();
+//           console.log("Order No: "+ this.orderNumber+" is delivered to "+this.customerName+" at "+this.customerAddress);
+//      }
+// }
+// orderData.customerMessageOnSuccessfullDelivery();
+
+
 let orderData = {
      orderNumber: 234566,
      customerName: "Robin",
      customerAddress: "HSR layout sector-4",
      customerMessageOnSuccessfullDelivery(){
           console.log("Order No: "+ this.orderNumber+" is delivered to "+this.customerName+" at "+this.customerAddress);
-          console.log(this);
-          
+          // console.log(this);
      }
 }
+// console.log(this);
+
 orderData.customerMessageOnSuccessfullDelivery();
+
+function paymentMode(){
+     console.log(this.customerName+" have to pay by cash");  
+}
+paymentMode.call(orderData);
+
+
+
