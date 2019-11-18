f9750f4ee90e7aa64c068561138c4afb57981105
Robin Hooda <robinhooda66@gmail.com>
2019-11-19 02:18:07 +0530

    returning vs mutating

---
+++ udemy-Learn JavaScript Full-Stack from Scratch/returning vs Mutating/index.js
@@ -0,0 +1,19 @@
+// returning vs mutating
+
+let students = [
+    { name:"Robin", qualification: "B.Tech", percentage:"64"},
+    { name:"Roshan", qualification: "BCA", percentage:"61"},
+    { name:"Rakesh", qualification: "MCA", percentage:"69"},
+    {name:"Ranjit",qualification:'MCA',percentage:"71"}
+];
+console.log(students);
+
+// console.log(students.push({name:"Rohan",qualification:'M.Tech',percentage:"72"}));
+students.push({name:"Rohan",qualification:'M.Tech',percentage:"72"});
+ 
+// students.foreach(y);
+// function y(x){
+//     console.log(`Name : ${x.name} have secured ${x.percentage} in ${x.qualification}`);
+// }
+console.log(students);
+
