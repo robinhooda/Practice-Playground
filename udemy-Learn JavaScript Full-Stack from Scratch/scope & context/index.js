b69a52cb24f15e99cf3ae7e30db534cdfd18d155
Robin Hooda <robinhooda66@gmail.com>
2019-11-19 03:27:49 +0530

    scope updated

--- udemy-Learn JavaScript Full-Stack from Scratch/scope & context/index.js
+++ udemy-Learn JavaScript Full-Stack from Scratch/scope & context/index.js
@@ -12,12 +12,16 @@
         // printName();
         // console.log(myName);
    // Scope to print different values Of myName 
-        let myName = "Robin";
-        function printName(){
-                let myName="Robin Hooda";
-                if(2+2==4){
-                        let myName=" Mr. Robin Hooda"
-                        console.log(myName);       
-                }        
-        }
-        printName();
+        // let myName = "Robin";
+        // function printName(){
+        //         let myName="Robin Hooda";
+        //         if(2+2==4){
+        //                 let myName=" Mr. Robin Hooda"
+        //                 console.log(`Inside the if statement, name is ${myName}`);       
+        //         }      
+        //         console.log(`Inside the function, name is  ${myName}`);       
+        // }
+        // console.log(`In the global scope, name is  ${myName}`);     
+        // printName();
+// let uses block scope
+// var uses function scope
