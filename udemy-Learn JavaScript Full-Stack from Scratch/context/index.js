1ce601d3e3ddd7e1d9e94ad01ecbf2ff44486adf
Robin Hooda <robinhooda66@gmail.com>
2019-11-20 02:06:36 +0530

    this using arrow function

--- udemy-Learn JavaScript Full-Stack from Scratch/context/index.js
+++ udemy-Learn JavaScript Full-Stack from Scratch/context/index.js
@@ -25,23 +25,39 @@
 // orderData.customerMessageOnSuccessfullDelivery();
 
 
+// let orderData = {
+//      orderNumber: 234566,
+//      customerName: "Robin",
+//      customerAddress: "HSR layout sector-4",
+//      customerMessageOnSuccessfullDelivery(){
+//           console.log("Order No: "+ this.orderNumber+" is delivered to "+this.customerName+" at "+this.customerAddress);
+//           // console.log(this);
+//      }
+// }
+// // console.log(this);
+
+// orderData.customerMessageOnSuccessfullDelivery();
+
+//call function
+     // function paymentMode(){
+     //      console.log(this.customerName+" have to pay by cash");  
+     // }
+     // paymentMode.call(orderData);
+
+
+// this using arrow function
+ 
 let orderData = {
      orderNumber: 234566,
      customerName: "Robin",
      customerAddress: "HSR layout sector-4",
      customerMessageOnSuccessfullDelivery(){
+         let tryThis = () =>console.log(this);
+          tryThis();
           console.log("Order No: "+ this.orderNumber+" is delivered to "+this.customerName+" at "+this.customerAddress);
-          // console.log(this);
      }
 }
-// console.log(this);
-
 orderData.customerMessageOnSuccessfullDelivery();
-//call function
-     function paymentMode(){
-          console.log(this.customerName+" have to pay by cash");  
-     }
-     paymentMode.call(orderData);
 
 
 
