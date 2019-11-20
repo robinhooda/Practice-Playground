71793aad724b50ee47e893467ba7cc3fefbbc234
Robin Hooda <robinhooda66@gmail.com>
2019-11-20 16:58:52 +0530

    form reset

--- udemy-Learn JavaScript Full-Stack from Scratch/to do App/index.js
+++ udemy-Learn JavaScript Full-Stack from Scratch/to do App/index.js
@@ -1,18 +1,19 @@
-
 let myForm = document.getElementById("myForm");
-let insertedItem = document.getElementById("insertedItem");
+let myField = document.getElementById("myField");
 let myList = document.getElementById("mylist");
 
 myForm.addEventListener("submit",(e) => {
      e.preventDefault();
      alert("Item added to the list"); 
-     addItem(insertedItem.value);  
-     myForm.reset();  
+     addItem(myField.value);  
+     // myForm.reset();  
 }) 
 
 function addItem(x){
      let myListItems = `<li> ${x}<button>Delete</button></li>`
      myList.insertAdjacentHTML("beforeend",myListItems)
+     myField.value = ""
+
 }
 
 
