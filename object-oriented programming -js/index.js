b8e97c29c3506af3e69d8deafc793f953720caeb
Robin Hooda <robinhooda66@gmail.com>
2019-12-19 10:48:57 +0530

    normal function

---
+++ object-oriented programming -js/index.js
@@ -0,0 +1,18 @@
+// Normal function using objects
+
+const circle={
+    radius: 1,
+    location: {
+        x: 1,
+        y:1
+    },
+    draw: function(){
+        console.log('draw');
+        
+    }
+};
+
+circle.draw();
+
+
+
