e79cb16f158729c55b869ca145794c33734bb648
Robin Hooda <robinhooda66@gmail.com>
2019-12-19 10:54:04 +0530

    factory function

--- object-oriented programming -js/index.js
+++ object-oriented programming -js/index.js
@@ -1,18 +1,35 @@
 // Normal function using objects
 
-const circle={
-    radius: 1,
-    location: {
-        x: 1,
-        y:1
-    },
-    draw: function(){
-        console.log('draw');
+// const circle={
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y:1
+//     },
+//     draw: function(){
+//         console.log('draw');
         
-    }
-};
+//     }
+// };
 
-circle.draw();
+// circle.draw();
 
+
+// Factory Function
+function createCircle(radius){
+    return{
+        radius,
+        draw: function(){
+            console.log('draw');   
+        }
+    };   
+}
+
+const circleWithRadiusOne = createCircle(1);
+const circleWithRadiusTwo = createCircle(2);
+
+circleWithRadiusOne.draw();
+console.log(circleWithRadiusOne.radius);
+console.log(circleWithRadiusTwo.radius);
 
 
